9b6bb29e35ff162cd6c39b0b02c30bf93eef4316
LeeEunSoo <supreme4rest@gmail.com>
2024-04-29 15:41:56 +0900

    네번째 커밋 나누기 완성...

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def mul_func(n1, n2) :
     retVal = n1 * n2
     return retVal
 
+def div_func(n1, n2) :
+    retVal = n1 / n2
+    return retVal
+
 ## 전역 변수 (=클래스 변수)
 num1, num2, res = 100, 200, 0
 ## 메인 함수 부분
@@ -24,3 +28,8 @@ print(num1, "-", num2, "=", res)
 
 res = mul_func(num1, num2)
 print(num1, "*", num2, "=", res)
+
+
+res = div_func(num1, num2)
+print(num1, "/", num2, "=", res)
+
