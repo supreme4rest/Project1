c0e1d80023e20ad89ec8c361be02ec3a6444a776
LeeEunSoo <supreme4rest@gmail.com>
2024-04-29 15:31:14 +0900

    첫번째 커밋 계산기 프로젝트 시작함 더하기 함수 완성함...

---
+++ main.py
@@ -0,0 +1,12 @@
+## 메서드 (=함수) 부분
+def add_func(n1, n2) :
+    retVal = n1 + n2
+    return retVal
+
+## 전역 변수 (=클래스 변수)
+num1, num2, res = 100, 200, 0
+
+
+## 메인 함수 부분
+res = add_func(num1, num2)
+print(num1, "+", num2, "=", res)
